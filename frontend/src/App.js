9427631a70dffa0ed73ef5af4ba8c404428c6967
mtrinh8@my.bcit.ca <mtrinh8@my.bcit.ca>
2022-02-19 21:43:16 -0800

    welcome 3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 import WelcomeWindow from './components/Welcome';
-import Btn from './components/Button';
 import HeaderNav from './components/Header';
 
 export default function App() {
   return (
     <div className="appCont">
-      <HeaderNav />
-
-      <WelcomeWindow />
-
-
-      {/* <Btn text="Get Started" /> */}
+      <div>
+        <HeaderNav />
+      </div>
+      <div className='welcomeCont'>
+        <WelcomeWindow />
+      </div>
     </div>
   );
 }
+
---
+++ frontend/src/routes/welcome.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class WelcomePage extends React.Component{
+  render() {
+    return (
+      
+    )
+  }
+}
