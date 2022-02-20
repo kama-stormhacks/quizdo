b544b8fb4936300cfade755ec0c1da04e4ce63c3
Kay Arellano <kay.arellano254@gmail.com>
2022-02-19 21:25:47 -0800

    Fix server port

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use("/user", user);
 
 console.log(process.cwd());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 server.listen(PORT, () => console.log(`Listening on ${PORT}`));
